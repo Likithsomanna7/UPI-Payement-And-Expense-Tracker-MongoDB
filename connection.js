86c9d544b1a2f7657b0d1dbcffc2d135d49a82d8
likith somanna <likithsomanna7@gmail.com>
2025-03-01 18:04:13 +0530

    Bug Fix

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 const  mongoose = require('mongoose');
 async function connected()
 {
-return mongoose.connect('mongodb://localhost:27017/UPI').then(()=>console.log("Sucessfully connected to the database"))
+return mongoose.connect('mongodb+srv://likithsomanna7:VWVR4BxHiZzOzdCi@upi-db.3ve9a.mongodb.net/?retryWrites=true&w=majority&appName=UPI-db').then(()=>console.log("Sucessfully connected to the database"))
                                                     .catch((err)=>{console.log(err)});
 }
 module.exports ={connected};
